3f6006cbec712bf6e51382df46513c4731102692
germangit <geralvaredo@gmail.com>
2017-04-17 00:18:02 -0300

    LOS PRIMEROS 20 NUMEROS PRIMOS

---
+++ Ejercicio24/funcion.js
@@ -0,0 +1,16 @@
+
+function mensaje()
+{
+    var contador = 0 ;
+    
+   
+    for (var index = 2; index < 20; index++) {
+        if(index%index==0 )
+        {
+            document.write(index + "<br>" );
+        }
+        
+    }
+ 
+} 
+
