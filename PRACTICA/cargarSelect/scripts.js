285fdaef9cccd694e56c706f4d8063bba984736c
German Alvaredo <german.alvaredo@naranja.com>
2023-10-16 16:46:12 -0300

    practica

---
+++ PRACTICA/cargarSelect/scripts.js
@@ -0,0 +1,20 @@
+Array.prototype.unique=function(a){
+    
+  });
+
+$(function(){
+
+    //cargar paises
+
+//agregar manejador para que cuando cambie el valor de pais, filtre la lista de ciudades	
+
+})
+
+function cargarPaises(){
+  
+}
+
+function cargarCiudades(pais){
+
+
+}
